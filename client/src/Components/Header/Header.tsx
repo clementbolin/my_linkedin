7b5b9ab8fe4ea477e87818d169da81732a57cc5c
ClementBolin <clement.bolin@epitech.eu>
2021-06-24 18:47:31 +0200

    add(master): NavBar design

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 import LogoLinkedin from './../../assets/home-logo.svg'
-import { AiOutlineSearch } from 'react-icons/ai'
+import UserLogo from './../../assets/user.svg'
+import { AiOutlineSearch, AiFillHome, AiFillMessage } from 'react-icons/ai'
+import { BsPeopleFill, BsGrid3X3Gap } from 'react-icons/bs'
+import { RiSuitcaseFill } from 'react-icons/ri'
+import { MdNotifications } from 'react-icons/md'
 
 export const Header = () => {
     return (
@@ -20,7 +24,55 @@ export const Header = () => {
                     </SearchIcon>
                 </Search>
                 <NavIcon>
-
+                    <ContentItem>
+                        <NavItem className='active'>
+                            <a href='/home'>
+                                <AiFillHome />
+                                <span>Home</span>
+                            </a>
+                        </NavItem>
+                        <NavItem>
+                            <a href='/home'>
+                                <BsPeopleFill />
+                                <span>Network</span>
+                            </a>
+                        </NavItem>
+                        <NavItem>
+                            <a href='/home'>
+                                <RiSuitcaseFill />
+                                <span>Jobs</span>
+                            </a>
+                        </NavItem>
+                        <NavItem>
+                            <a href='/home'>
+                                <AiFillMessage />
+                                <span>Messaging</span>
+                            </a>
+                        </NavItem>
+                        <NavItem>
+                            <a href='/home'>
+                                <MdNotifications />
+                                <span>Notifications</span>
+                            </a>
+                        </NavItem>
+                        <User>
+                            <a href="/home">
+                                <img src={UserLogo} alt="usr logo" />
+                                <span>Me</span>
+                            </a>
+                            <SignOut>
+                                <a href='/'>
+                                    Sign Out
+                                </a>
+                            </SignOut>
+                        </User>
+                        <Work>
+                            <a href="/home">
+                                <BsGrid3X3Gap />
+                                <span>Products</span>
+                            </a>
+                        </Work>
+                    </ContentItem>
                 </NavIcon>
             </Content>
         </Container>
@@ -44,7 +96,7 @@ const Content = styled.div`
     margin: 0 auto;
     min-height: 100%;
     max-width: 1120px;
-    padding: 6px 4px;
+    padding: 0px 3px;
 `
 
 const Logo = styled.span`
@@ -67,7 +119,7 @@ const Search = styled.div`
             background-color: #eef3f8;
             border-radius: 2px;
             color: rgba(0, 0, 0, 0.9);
-            width: 210px;
+            width: 260px;
             height: 34px;
             padding: 0 8px 0 40px;
             line-height: 1.75;
@@ -85,5 +137,116 @@ const SearchIcon = styled.div`
     left: 10px;
 `
 
-const NavIcon = styled.div`
+const NavIcon = styled.nav`
+    margin-left: auto;
+    display: block;
+    @media (max-width: 768px) {
+        left: -100;
+        bottom: 0;
+        width: 100%;
+        background-color: white;
+        position: fixed;
+    }
+`
+
+const ContentItem = styled.ul`
+    display: flex;
+    flex-wrap: nowrap;
+    list-style-type: none;
+
+    .active {
+        span:after {
+            content: "";
+            transform: scaleX(1);
+            border-bottom: 2px solid var(--white, #fff);
+            bottom: 0;
+            left: 0;
+            position: absolute;
+            transition: transform 0.2s ease-in-out;
+            width: 100%;
+            border-color: rgba(0, 0, 0, 0.9);
+        }
+    }
+`
+
+const NavItem = styled.li`
+    display: flex;
+    align-items: center;
+    padding-top: 3px;
+    a {
+        align-items: center;
+        background: transparent;
+        display: flex;
+        flex-direction: column;
+        font-size: 22px;
+        font-weight: 400;
+        justify-content: center;
+        line-height: 1.8;
+        min-height: 42px;
+        min-width: 80px;
+        position: relative;
+        text-decoration: none;
+        color: black;
+    }
+
+    @media (max-width: 768px) {
+      min-width: 90px;
+    }
+
+    span {
+        font-size: 11px;
+        color: rgba(0, 0, 0, 0.6);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    &:hover, &:active {
+        a {
+            span {
+                color: rgba(0, 0, 0, 0.9);
+            }
+        }
+    }
+`
+
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    text-align: center;
+    display: none;
+`
+
+const User = styled(NavItem)`
+    a > svg {
+        width: 24px;
+        border-radius: 50%;
+    }
+
+    a > img {
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+    }
+
+    &:hover {
+        ${SignOut} {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+`
+
+const Work = styled(User)`
+    a > svg {
+        border-radius: initial;
+    }
+    border-left: 1px solid rgba(0, 0, 0, 0.08);
 `
